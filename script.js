33014a7a168be5529b25d91004a3228d6359433b
anelizsuarez123 <90357763+palomasuarez@users.noreply.github.com>
2021-10-20 22:30:27 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let reto ={
+  NOMBRE :'PALOMA ANELIZ',
+APELLIDO : 'HERNANDEZ SUAREZ',
+EDAD : '13 ',
+GRADO : '2do '
+};
+function ACTIVIDAD (reto){
+  console.log("Hola soy",reto.NOMBRE , reto.APELLIDO,"Tengo",reto.EDAD,"años" ,"Y estoy en" , reto.GRADO,"año" )
+}
+ ACTIVIDAD (reto);
